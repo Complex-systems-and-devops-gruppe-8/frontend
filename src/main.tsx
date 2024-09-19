7c39b1cd0a5afb5fae0e8c0e31c7a6ceafeb816b
Johan Sogaard <Johansogaardjorgensen@gmail.com>
2024-09-19 13:53:01 +0200

    routing works for 3 pages (blackjack, poker and coinflip)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';  
 import LandingPage from './Landingpage';
+import PokerPage from './Pages/Poker';
+import CoinFlipPage from './Pages/CoinFlip';
+import BlackjackPage from './Pages/Blackjack';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -9,6 +12,10 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/coinflip" element={<CoinFlipPage />} />
+        <Route path="/poker" element={<PokerPage />} />
+        <Route path="/blackjack" element={<BlackjackPage />} />
+
       </Routes>
     </Router>
   </React.StrictMode>
