c7c894fb23b48eac226e4bd882aa0ae9344fe150
muItilingualism <sebhalfdan@gmail.com>
2024-11-07 12:55:42 +0100

    Add Siren API POC

--- src/main.tsx
+++ src/main.tsx
@@ -7,21 +7,23 @@ import CoinFlipPage from './Pages/CoinFlip';
 import BlackjackPage from './Pages/Blackjack';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
 import { UserProvider } from './state/userState/userContext'; 
+import { AuthProvider } from './contexts/AuthContext';
+import { LoginPage } from './components/LoginPage';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
       {/*Router is used to navigate between pages in the app :O*/}
       <Router>
         {/*gives access to the users state in all inclosed components :D*/}
-        <UserProvider>
+        <AuthProvider>
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/coinflip" element={<CoinFlipPage />} />
             <Route path="/poker" element={<PokerPage />} />
             <Route path="/blackjack" element={<BlackjackPage />} />
-           
+            <Route path="/login" element={<LoginPage />} />
           </Routes>
-        </UserProvider>
+        </AuthProvider>
     </Router>
   </React.StrictMode>
 );
